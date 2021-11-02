782fcbdf0b16bc12441ebc6d596968deddd651df
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-11-02 15:45:35 -0400

    show the restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :restaurants
   resources :foods
   # get 'home/index'
   root 'home#index'
---
+++ db/migrate/20211102193525_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :restaurant_name
+      t.string :address
+      t.string :cuisine
+
+      t.timestamps
+    end
+  end
+end
