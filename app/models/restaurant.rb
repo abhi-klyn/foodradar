c0733b4e368ad1bcb6e73b67f3057de2be4b8312
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-11-02 16:14:00 -0400

    moved search by restaurant to index, clicking show redirects to menu page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  def self.search(search)
+    if not search.nil?
+      puts("Here")
+      self.where(restaurant_name: search)
+    else
+      Restaurant.all
+    end
+  end
 end
