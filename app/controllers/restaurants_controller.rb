ed9dadadb3b7d0934e02bbefce00faf6a2f05957
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-11-02 16:01:03 -0400

    clicking on restaurant's show, displays the menu of the restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1 or /restaurants/1.json
   def show
+    @foods = Food.searchByRestaurant(@restaurant[:restaurant_name])
   end
 
   # GET /restaurants/new
--- app/models/food.rb
+++ app/models/food.rb
@@ -8,4 +8,13 @@ class Food < ApplicationRecord
       Food.all
     end
   end
+
+  def self.searchByRestaurant(restaurant)
+    if not restaurant.nil?
+      puts("Here")
+      self.where(restaurant_name: restaurant)
+    else
+      return nil
+    end
+  end
 end
