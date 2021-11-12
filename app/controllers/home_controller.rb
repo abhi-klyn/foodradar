993b3f6caebb755797ae71f22d8e1a5786b66e40
Brian Paick <brian19977@github.com>
2021-11-12 14:29:25 -0500

    substring search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,64 @@ class HomeController < ApplicationController
     #if params.key?(:food_name)
     #  @foods = Food.search(params[:food_name])
     #else
-      @restaurants = Restaurant.search(params[:restaurant_name])
+    
+    #puts "printing var restaurants"
+    #puts @restuarants
     #end
+    '''
     records = execute_statement("SELECT * FROM restaurants")
     records.each do |row|
         puts row
+    
+    puts params[:restaurant_name]
+    @records
     end
+    '''
+
+    
+    @restaurants = nil
+    if params[:restaurant_name] && params[:restaurant_name] != ""
+      puts "here"
+      
+      
+      restaurant_list = []
+      records = execute_statement("SELECT DISTINCT restaurant_name FROM restaurants WHERE LOWER(restaurant_name) LIKE LOWER('%"+params[:restaurant_name]+"%')")
+      if records
+        records.each do |row|
+          restaurant_list.push(Restaurant.search(row.fetch("restaurant_name")))
+        end
+      
+        len = records.length()
+        puts len
+        @restaurants = Restaurant.search(records[0].fetch("restaurant_name"))
+
+        for i in 1..len-1 do
+          @restaurants = @restaurants.or(Restaurant.search(records[i].fetch("restaurant_name")))
+        end
+
+      else
+        @restaurants = Restaurant.search(params[:restaurant_name])
+      end
+
+    elsif params[:restaurant_name] == ""
+      puts "elsif"
+      params[:restaurant_name] = nil
+      @restaurants = Restaurant.search(params[:restaurant_name])
+    else
+      puts "else"
+      @restaurants = Restaurant.search(params[:restaurant_name])
+      puts "class"
+      puts @restaurants.class
+      puts "class"
+      puts @restaurants
+    end
+    
+    
+    
+    
+
+    
+
+
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   def self.search(search)
     if not search.nil?
       self.where(restaurant_name: search)
+
     else
       Restaurant.all
     end
